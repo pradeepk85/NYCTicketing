f84432c33bd33c3c3d6ad79b5e3397cd6a4f812c
Pradeep Khandelwal <home@Pradeeps-MBP.fios-router.home>
2018-04-18 23:49:25 -0400

    Adding code to generate histogram

--- generate_graph.py
+++ generate_graph.py
@@ -1,20 +1,29 @@
 import csv
 import json
 import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
+from matplotlib import cm
 import numpy as np
 from pprint import pprint
 
 def GenerateHistogram(xPoints, yPoints):
     x = np.arange(len(xPoints))
-    plt.bar(x, height= yPoints)
+    cs=cm.Set1(np.arange(40)/20.)
+    plt.bar(x, height= yPoints,  color=cs)
     plt.xticks(x+.5, xPoints)
     plt.grid(True)
-    plt.savefig("test.png")
+    plt.savefig("test1.png")
 
 
-def ReadJson(fileName):
-    dataPoints = json.load(open(fileName))
-    return dataPoints
+def GeneratePieChart(xPoints, yPoints):
+    labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
+    fracs = [15, 30, 45, 10]
+    the_grid = GridSpec(1, 1)
+    plt.subplot(the_grid[0, 0], aspect=1)
+    cs=cm.Set1(np.arange(40)/5.)
+    plt.pie(fracs, labels=labels, autopct='%1.1f%%', colors=cs, shadow=True)
+    plt.savefig("test2.png")
+
 
 def ReadCSV(fileName, noOfRows):
     carType = []
@@ -33,3 +42,4 @@ def ReadCSV(fileName, noOfRows):
 
 carType, carCount = ReadCSV("parking_data.csv", 10)
 GenerateHistogram(carType, carCount)
+GeneratePieChart(carType, carCount)
